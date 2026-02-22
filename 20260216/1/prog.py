116dc1f6f4082361fbb397c838a53def8a644847
Anya <anya@MacBook-Pro-Anya.local>
2026-02-22 10:25:25 +0300

    add function for view last commit

--- 20260216/1/prog.py
+++ 20260216/1/prog.py
@@ -16,7 +16,35 @@ def names_br(path):
         br.append(os.path.basename(i))
     return '\n'.join(br)
 
+# Вывести объект-последний коммит (последний коммит указанной ветки).
+# tree 3182cbd0e8b2baecbdc693008ed832b25546e225
+# parent f97190bd4dea21041d1cd7fcf822cd4ac6e7716b
+# author Ivan Samovarov <isamovarov@mail.ru>
+# committer Elec Tropoezd <elec@tropoe.zd>
+
+# Added a very important feature.
+
+def last_comm(path, name_br):
+    file = path + "/.git/refs/heads/" + name_br
+    with open(file, "r") as f:
+        id = f.read()[:-1]
+    
+    ob = f'{path}/.git/objects/{id[:2]}/{id[2:]}'
+
+    with open(ob, "rb") as f:
+        ob = zlib.decompress(f.read())
+    
+    head, _, body = ob.partition(b'\x00')
+    kind, _ = head.split()
+
+    print(id, kind.decode())
+    print(body.decode())
+    return
+
 
 path = sys.argv[1]
 if len(sys.argv) == 2:
     print(names_br(path))
+    exit
+name_br = sys.argv[2]
+last_comm(path, name_br)
