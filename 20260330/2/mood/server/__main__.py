1c76e668979bf6966faac08b5c237296ccff02d2
Anya <anya@MacBook-Pro-Anya.local>
2026-04-03 10:06:45 +0300

    start mood

---
+++ 20260330/2/mood/server/__main__.py
@@ -0,0 +1,236 @@
+import sys
+import cmd
+import io
+import socket
+import shlex
+import cowsay
+import readline
+import asyncio
+
+JGSBAT = cowsay.read_dot_cow(io.StringIO(r"""
+$the_cow = <<EOC;
+    ,_                    _,
+    ) '-._  ,_    _,  _.-' (
+    )  _.-'.|\\--//|.'-._  (
+     )'   .'\/o\/o\/'.   `(
+      ) .' . \====/ . '. (
+       )  / <<    >> \  (
+        '-._/``  ``\_.-'
+  jgs     \\'--'//
+         (((""  "")))
+EOC
+"""))
+
+class Player:
+    '''class for each player'''
+    def __init__(self):
+        '''init player'''
+        self.x = 0
+        self.y = 0
+
+    def move(self, x1, y1):
+        '''move player to x, y '''
+        self.x = (self.x + x1) % 10
+        self.y = (self.y + y1) % 10
+        return self.x, self.y
+
+
+class Monster:
+    '''class for each monster'''
+    def __init__(self, name, word, hp):
+        '''init monster'''
+        self.name = name
+        self.word = word
+        self.hp = hp
+
+
+class Game:
+    '''The interaction class of players, monsters, and so on'''
+    def __init__(self):
+        '''make field and dict of players'''
+        self.field = [[None] * 10 for _ in range(10)]
+        self.players = {}
+
+    def add_player(self, name):
+        '''add player to game'''
+        self.players[name] = Player()
+
+    def remove_player(self, name):
+        '''remove player from game'''
+        del self.players[name]
+
+    def move_player(self, player_name, x1, y1):
+        '''move player to x,y'''
+        x, y = self.players[player_name].move(x1, y1)
+        monster = self.field[x][y]
+
+        res = f"Moved to ({x}, {y})"
+        if monster:
+            if monster.name == "jgsbat":
+                res += "\n" + cowsay.cowsay(monster.word, cowfile=JGSBAT)
+            else:
+                res += "\n" + cowsay.cowsay(monster.word, cow=monster.name)
+        return res
+
+    def addmon(self, name, x, y, hp, word):
+        '''add element of class Monster'''
+        replaced = self.field[x][y] is not None
+        self.field[x][y] = Monster(name, word, hp)
+        return replaced
+
+    def attack(self, player_name, monster_name, damage):
+        '''attack monster with weapon'''
+        player = self.players[player_name]
+        monster = self.field[player.x][player.y]
+
+        if not monster or monster.name != monster_name:
+            return False, 0, 0
+
+        damage_done = min(monster.hp, damage)
+        monster.hp -= damage_done
+        hp_left = monster.hp
+
+        if hp_left == 0:
+            self.field[player.x][player.y] = None
+
+        return True, damage_done, hp_left
+
+
+class Client:
+    '''class of player'''
+    def __init__(self, writer):
+        '''init player'''
+        self.writer = writer
+        self.queue = asyncio.Queue()
+
+
+clients = {}
+game = Game()
+
+
+async def send_msgs(me):
+    '''send messages from client'''
+    while True:
+        msg = await me.queue.get()
+        me.writer.write((msg + "\0").encode())
+        await me.writer.drain()
+
+
+async def broadcast(msg, skip=None):
+    '''sending messages to everyone'''
+    for name, client in clients.items():
+        if name != skip:
+            await client.queue.put(msg)
+
+
+async def echo_client(reader, writer):
+    '''connecting the client'''
+    addr = writer.get_extra_info("peername")
+
+    data = await reader.readline()
+    if not data:
+        writer.close()
+        await writer.wait_closed()
+        return
+
+    name = data.decode().rstrip("\n")
+
+    if name in clients:
+        writer.write("Username is already taken\n".encode())
+        await writer.drain()
+        writer.close()
+        await writer.wait_closed()
+        return
+
+    me = Client(writer)
+    clients[name] = me
+    game.add_player(name)
+
+    print(f"Connected: {addr} as {name}")
+
+    writer.write(f"Hello, {name}\n".encode())
+    await writer.drain()
+
+    sender = asyncio.create_task(send_msgs(me))
+    await broadcast(f"{name} entered the MUD")
+
+    while not reader.at_eof():
+        data = await reader.readline()
+        if not data:
+            break
+
+        args = shlex.split(data.decode().rstrip("\n"))
+        if not args:
+            continue
+
+        cmd = args[0]
+
+        if cmd == "move":
+            x1, y1 = int(args[1]), int(args[2])
+            response = game.move_player(name, x1, y1)
+            await me.queue.put(response)
+
+        elif cmd == "addmon":
+            mon_name = args[1]
+            word = args[1 + args.index("hello")]
+            hitpoints = int(args[1 + args.index("hp")])
+            c_id = args.index("coords")
+            x = int(args[c_id + 1])
+            y = int(args[c_id + 2])
+
+            replaced = game.addmon(mon_name, x, y, hitpoints, word)
+
+            response = f"{name} added monster {mon_name} with {hitpoints} hp"
+            if replaced:
+                response += "\nReplaced the old monster"
+
+            await broadcast(response)
+
+        elif cmd == "attack":
+            mon_name = args[1]
+            weapon = args[2]
+            damage = int(args[3])
+
+            ok, damage_done, hp_left = game.attack(name, mon_name, damage)
+
+            if not ok:
+                await me.queue.put(f"No {mon_name} here")
+                continue
+
+            response = (
+                f"{name} attacked {mon_name} with {weapon}, "
+                f"damage {damage_done} hp"
+            )
+            if hp_left == 0:
+                response += f"\n{mon_name} died"
+            else:
+                response += f"\n{mon_name} now has {hp_left} hp"
+
+            await broadcast(response)
+            
+        elif cmd == "sayall":
+            msg = args[1]
+            await broadcast(f"{name}: {msg}")
+
+        else:
+            await me.queue.put("Invalid command")
+
+    print(f"Disconnected: {addr} as {name}")
+    del clients[name]
+    game.remove_player(name)
+    await broadcast(f"{name} left the MUD")
+    sender.cancel()
+    writer.close()
+    await writer.wait_closed()
+
+
+async def main():
+    '''main'''
+    server = await asyncio.start_server(echo_client, "0.0.0.0", 1337)
+
+    async with server:
+        await server.serve_forever()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
