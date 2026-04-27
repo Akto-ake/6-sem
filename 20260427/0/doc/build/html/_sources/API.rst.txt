5d19cd593f9deb4ca55351ac8a22290adcb7f4dd
Anya <anya@MacBook-Pro-Anya.local>
2026-04-27 14:19:30 +0300

    class work dodo

--- 20260427/0/doc/build/html/_sources/API.rst.txt
+++ 20260427/0/doc/build/html/_sources/API.rst.txt
@@ -3,4 +3,8 @@ API
 
 Here *will* be **API**
 
+.. automodule:: AppBase
+   :members:
+   :private-members:
+
 .. image:: _static/images.jpeg
--- 20260427/0/dodo.py
+++ 20260427/0/dodo.py
@@ -1,6 +1,40 @@
+from pathlib import Path
+from zipfile import ZipFile
+
+DOIT_CONFIG = {"default_tasks": ['docs']}
+
+
 def task_docs():
     """"Create  documentation"""
-
+    rstpy = list(Path(".").glob("**/*.rst")) + list(Path(".").glob("**/*.py")) 
+    # print(rstpy)
+    ext = {"html": "html", "text": "text"}
+    for typ in ("html", "text"):
+        yield {
+            "name": f"{typ} doc",
+            "actions": [f"cd doc && sphinx-build -M {typ} source source/_build"],
+            "targets": [f"doc/source/_build/html/index.{ext[typ]}"],
+            "file_dep": rstpy,
+        }
+    
+def task_erase():
+    """Clean all junk"""
+    
     return {
-        "actions": ["cd doc && sphinx-build -M html . _build"],
+        "actions": ["rm -rf doc/source/_build *.zip"],
+    }
+
+def task_zip():
+    """Create zip archive"""
+    def create_zip(filename, files):
+        with (ZipFile(filename, "w") as zf):
+            for f in files:
+                zf.write(f)
+    
+    
+    files = list(Path("doc/source/_build/html").glob("**"))
+    
+    return {
+        "actions": [(create_zip, ["docs.zip", files])],
+        "task_dep": ["docs"],
     }
