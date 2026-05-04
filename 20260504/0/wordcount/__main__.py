a83aa323ab7067563398277a1c8bdb336fd430b6
Anya <anya@MacBook-Pro-Anya.local>
2026-05-04 14:18:38 +0300

    add scripts

--- 20260504/0/wordcount/__main__.py
+++ 20260504/0/wordcount/__main__.py
@@ -1,11 +1,3 @@
-import gettext
-import locale
-from . import PATH
+from . import main
 
-locale = locale.setlocale(locale.LC_ALL, locale.getlocale())
-translation = gettext.translation("wordcount", PATH, fallback=True)
-ngettext = translation.ngettext
-
-words = input().split()
-n = len(words)
-print(ngettext("Entered {} word", "Entered {} words", n).format(n))
+main()
