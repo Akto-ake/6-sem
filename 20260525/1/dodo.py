cc7a2a9b13b4e287aec6910aeefbf36108d8d29c
Anya <anya@MacBook-Pro-Anya.local>
2026-05-25 07:03:09 +0300

    ex

---
+++ 20260525/1/dodo.py
@@ -0,0 +1,78 @@
+from pathlib import Path
+import shutil
+
+from doit.task import clean_targets
+
+
+DOIT_CONFIG = {"default_tasks": ["html"]}
+
+def task_pot():
+    """Build translation"""
+    return {
+        "file_dep": list(Path("mood").glob("**/*.py")),
+        "actions": ["python3 -m babel.messages.frontend extract -o mud.pot mood"],
+        "targets": ["mud.pot"],
+        "clean": [clean_targets],
+    }
+
+
+def task_po():
+    """Update translation"""
+    return {
+        "file_dep": ["mud.pot"],
+        "actions": [
+            "python3 -m babel.messages.frontend update "
+            "-D mud -i mud.pot -d po -l ru_RU.UTF-8 --init-missing",
+        ],
+        "targets": ["po/ru_RU.UTF-8/LC_MESSAGES/mud.po"],
+        "task_dep": ["pot"],
+        "clean": [clean_targets],
+    }
+
+
+def task_mo():
+    """Compile translation"""
+    return {
+        "file_dep": ["po/ru_RU.UTF-8/LC_MESSAGES/mud.po"],
+        "actions": [
+            "mkdir -p mood/po/ru_RU.UTF-8/LC_MESSAGES",
+            "python3 -m babel.messages.frontend compile "
+            "-D mud -i po/ru_RU.UTF-8/LC_MESSAGES/mud.po "
+            "-o mood/po/ru_RU.UTF-8/LC_MESSAGES/mud.mo",
+        ],
+        "targets": ["mood/po/ru_RU.UTF-8/LC_MESSAGES/mud.mo"],
+        "task_dep": ["po"],
+        "clean": [clean_targets],
+    }
+
+
+def task_i18n():
+    """Build i18n"""
+    return {
+        "actions": ["touch .i18n"],
+        "targets": [".i18n"],
+        "task_dep": ["pot", "po", "mo"],
+        "clean": [clean_targets],
+    }
+
+
+def task_html():
+    """Build html"""
+    rstpy = list(Path("doc").glob("**/*.rst")) + list(Path("mood").glob("**/*.py")) + [Path("doc/conf.py")]
+    return {
+        "actions": ["sphinx-build -M html doc mood/doc"],
+        "targets": ["mood/doc/html/index.html"],
+        "file_dep": rstpy,
+        "clean": [(shutil.rmtree, ["mood/doc"], {"ignore_errors": True})],
+    }
+
+
+def task_test():
+    """Run tests"""
+    return {
+        "file_dep": ["mood/po/ru_RU.UTF-8/LC_MESSAGES/mud.mo", "test_server.py"],
+        "actions": ["python3 -m unittest test_server.py", "touch .test"],
+        "targets": [".test"],
+        "task_dep": ["i18n"],
+        "clean": [clean_targets],
+    }
